29fb4bfca8cae279f54c7ad939056039e9564f97
Brendan Ryan <brendan.james.ryan@gmail.com>
2023-01-22 10:24:25 -0500

    course complete

---
+++ server.js
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+    });
+
+    const image = aiResponse.data.data[0].url;
+    res.send({ image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
